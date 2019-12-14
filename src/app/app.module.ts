7e3a9f1f9682ed94d4c82652ecc46738fe936b38
Ajay Marathe <ajay.marathe@xebecindia.com>
2019-12-14 15:20:08 +0530

    working on hightlight.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { NgMaterialIconModule } from 'ng-material-icon';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { DataTablesModule } from 'angular-datatables';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,14 @@ import { AdminComponent } from './admin/admin.component';
 import { RegisterComponent } from './auth/register/register.component';
 import {LoginComponent} from './auth/login/login.component';
 import { LimitToPipe } from './limit-to.pipe';
+
+export function getHighlightLanguages() {
+  return {
+    javascript: () => import('highlight.js/lib/languages/javascript'),
+    css: () => import('highlight.js/lib/languages/css'),
+    xml: () => import('highlight.js/lib/languages/xml')
+  };
+}
 
 @NgModule({
   declarations: [
@@ -39,8 +48,16 @@ import { LimitToPipe } from './limit-to.pipe';
     NgMaterialIconModule,
     DataTablesModule,
     CKEditorModule,
+    HighlightModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        languages: getHighlightLanguages()
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
